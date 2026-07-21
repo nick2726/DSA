960b038733c21dccef8b6ab189c18fb6706a6c7b
Nikhil Kumar Jha <nikhilnkj2627@gmail.com>
2026-07-21 17:53:27 +0530

    Time: 0 ms (100%), Space: 10.3 MB (24.54%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -18,3 +18,23 @@ public:
         return true;
     }
 };
+
+
+/*
+
+
+class Solution {
+   public:
+    bool isPalindrome(string s) {
+        string str = "";
+        for (char ch : s) {
+            if (isalnum(ch)) {
+                str += tolower(ch);
+            }
+        }
+        string reversed(str.rbegin(), str.rend());
+        return str == string(str.rbegin(), str.rend());
+    }
+};
+*/
+
